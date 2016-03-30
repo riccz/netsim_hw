16a09edcc9f5f443b730bb668f386325aef78c17
Riccardo Zanol <ricc@zanol.eu>
2016-03-31 00:07:49 +0200

    Output +-inf when no pred.int is possible

--- hw1/matlab/prediction_interval_bootstrap.m
+++ hw1/matlab/prediction_interval_bootstrap.m
@@ -3,7 +3,10 @@ sorted_x = sort(x);
 n = length(x);
 alpha = 1- gamma;
 if alpha < 2/(n+1)
-    error('cannot compute a prediction interval at this level of confidence');
+    warning('cannot compute a prediction interval at this level of confidence');
+    low = -inf;
+    upp = +inf;
+    return;
 end
 low = sorted_x(floor((n+1)*alpha/2));
 upp = sorted_x(ceil((n+1)*(1-alpha/2)));
