a3c4aa9130adf7fe7ab5b1ed6ab141236aed453d
Riccardo Zanol <ricc@zanol.eu>
2016-04-28 18:54:26 +0200

    Fix part 2

--- hw2/matlab/part2.m
+++ hw2/matlab/part2.m
@@ -2,12 +2,12 @@ close all; clear all; clc;
 
 N = 1e5;
 ns = [10, 50, 100, 200, 1000];
-ps = [0.01, 0.5, 0.9];
+ps = [1e-4 0.01, 0.5, 0.9];
 times = zeros(length(ns), length(ps), 3);
 
 for i=1:length(ns)
     for j=1:length(ps)
-        if (ns(i) <= 200)
+        if ~(ns(i) == 1000 && ps(j) == 0.9)
             tic;
             x_cdfinv = binomial_cdfinv(ns(i), ps(j), N);
             times(i, j, 1) = toc;
@@ -19,12 +19,12 @@ for i=1:length(ns)
         times(i, j, 2) = toc;
         fprintf('.');
         
-        if ns(i) <= 100 || (ps(j) <= 0.01 && ns(i) <= 200)
+        %if ns(i) <= 100 || (ps(j) <= 0.01 && ns(i) <= 200)
             tic;
             x_geozero = binomial_geozero(ns(i), ps(j), N);
             times(i, j, 3) = toc;
             fprintf('.');
-        end
+        %end
         
         fprintf('\n');
     end
@@ -35,7 +35,7 @@ fprintf('Binomial generation timing:\n');
 for i=1:length(ns)
     for j=1:length(ps)
         fprintf('n = %d & ', ns(i));
-        fprintf('p = %.2f & ', ps(j));
+        fprintf('p = %.4f & ', ps(j));
         fprintf('\\SI{%.3f}{\\s} & ', times(i, j, 1));
         fprintf('\\SI{%.3f}{\\s} & ', times(i, j, 2));
         fprintf('\\SI{%.3f}{\\s} ', times(i, j, 3));
