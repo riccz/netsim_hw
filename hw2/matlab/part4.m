a88e9cfba6c453176cd7899daa8c535a4037b2b7
Riccardo Zanol <ricc@zanol.eu>
2016-05-01 22:53:08 +0200

    Update part 4

--- hw2/matlab/part4.m
+++ hw2/matlab/part4.m
@@ -4,17 +4,11 @@ a1 = 18;
 a2 = 2;
 m = 101;
 
-lcg1 = lcg(a1, 0, m, 1);
-lcg2 = lcg(a2, 0, m, 1);
-
 notfullperiod1 = [];
 notfullperiod2 = [];
 for seed=1:m-1
-    lcg1.reset(seed);
-    lcg2.reset(seed);
-    
-    rand1 = lcg1.next(m-1);
-    rand2 = lcg2.next(m-1);
+    rand1 = lcg(a1, 0, m, seed, m-1);
+    rand2 = lcg(a2, 0, m, seed, m-1);
     
     [x1,i1] = find(histcounts(rand1, m-1) > 1);
     if ~isempty(x1)
@@ -24,29 +18,29 @@ for seed=1:m-1
     [x2, i2] = find(histcounts(rand2, m-1) > 1);
     if ~isempty(x2)
         notfullperiod2 = [notfullperiod2 seed];
-    end        
+    end
 end
 
 if isempty(notfullperiod1)
     fprintf('LCG1 is full period\n');
+else
+    fprintf('LCG1 is not full period\n');
 end
 
 if isempty(notfullperiod2)
     fprintf('LCG2 is full period\n');
+else
+    fprintf('LCG2 is not full period\n');
 end
 
-lcg1.reset(1);
-u = lcg1.next(m-1);
-lcg1.reset(u(1));
-u_next = lcg1.next(m-1);
+[u1, x1] = lcg(a1, 0, m, 1, m-1);
+u1_shift = lcg(a1, 0, m, x1(2), m-1);
 
 figure;
-scatter(u, u_next, '.');
+scatter(u1, u1_shift, '.');
 
-lcg2.reset(1);
-u = lcg2.next(m-1);
-lcg2.reset(u(1));
-u_next = lcg2.next(m-1);
+[u2, x2] = lcg(a2, 0, m, 1, m-1);
+u2_shift = lcg(a2, 0, m, x2(2), m-1);
 
 figure;
-scatter(u, u_next, '.');
+scatter(u2, u2_shift, '.');
