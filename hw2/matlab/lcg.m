9851ceacc6d2b6380cf095bf800b2c203c7c5c76
Riccardo Zanol <ricc@zanol.eu>
2016-05-01 22:51:28 +0200

    Replace the lcg class with a stateless function

--- hw2/matlab/lcg.m
+++ hw2/matlab/lcg.m
@@ -1,42 +1,13 @@
-classdef lcg < handle
-    properties (SetAccess = private)
-        a
-        b
-        m
-        seed
-    end
-    
-    properties (Access = protected)
-        state
-    end
-    
-    methods
-        function obj = lcg(a, b, m, seed)
-            if nargin < 4
-                seed = 1;
-            end
-            obj.a = a;
-            obj.b = b;
-            obj.m = m;
-            obj.seed = seed;
-            obj.state = seed;
-        end
-        
-        function v = next_one(obj)
-            obj.state = mod(obj.state * obj.a + obj.b, obj.m);
-            v = obj.state / obj.m;
-        end
-        
-        function vs = next(obj, N)
-            vs = zeros(N,1);
-            for i=1:N
-                vs(i) = next_one(obj);
-            end
-        end
-        
-        function reset(obj, seed)
-            obj.seed = seed;
-            obj.state = seed;
-        end
-    end
+function [u, x] = lcg(a, b, m, seed, N)
+assert(m > 0);
+assert(a < m);
+assert(b < m);
+assert(seed < m);
+
+x = zeros(N, 1);
+x(1) = seed;
+for i=2:N
+    x(i) = mod(x(i-1) * a + b, m);
+end
+u = x ./ m;
 end
