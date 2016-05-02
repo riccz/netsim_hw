3ed347a7156dfef0b408f7ab4efc4dfc84f55013
Riccardo Zanol <ricc@zanol.eu>
2016-05-02 17:55:42 +0200

    Update part 5

--- hw2/matlab/part5.m
+++ hw2/matlab/part5.m
@@ -5,31 +5,23 @@ m = 2^31;
 
 figure(1);
 hold on;
-    grid on;
-    figure(2);
+        figure(2);
     hold on;
-    grid on;
-    
-chunksize = m/128;
+        
+chunksize = m/2048;
 iters = ceil((m-1) / chunksize);
 last_x = 1;
 for i=1:iters
-    [u, x] = lcg(a, 0, m, last_x, chunksize+1);
-    u = u(1:chunksize);
-    
-    if find(histcounts(u, chunksize) > 1)
-        error('not full period!!!!!!');
-    end
-    
-    u_shift = lcg(a, 0, m, x(2), chunksize);
-    u_shift_shift = lcg(a, 0, m, x(3), chunksize);
+    [u, x] = lcg(a, 0, m, last_x, chunksize+2);
+    u1 = u(1:chunksize);
+    u2 = u(2:chunksize+1);
+    u3 = u(3:chunksize+2);
     
     figure(1);    
-    scatter(u, u_shift, 'b.');
+    scatter(u1, u2, 'b.');
     
     figure(2);    
-    scatter3(u, u_shift, u_shift_shift, 'b.');
+    scatter3(u1, u2, u3, 'b.');
     
     last_x = x(chunksize+1);
-    pause;
 end
