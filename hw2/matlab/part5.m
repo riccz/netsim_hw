3c803746dd2d4d623e4b15c7759491927f983783
Riccardo Zanol <ricc@zanol.eu>
2016-05-02 18:36:55 +0200

    Update part 5

--- hw2/matlab/part5.m
+++ hw2/matlab/part5.m
@@ -5,23 +5,32 @@ m = 2^31;
 
 figure(1);
 hold on;
-        figure(2);
-    hold on;
-        
-chunksize = m/2048;
+figure(2);
+hold on;
+view(-36, 22);
+
+chunksize = 1000;
 iters = ceil((m-1) / chunksize);
 last_x = 1;
 for i=1:iters
+    if i == iters
+        chunksize = m-1 - (i-1)*chunksize;
+    end
     [u, x] = lcg(a, 0, m, last_x, chunksize+2);
     u1 = u(1:chunksize);
     u2 = u(2:chunksize+1);
     u3 = u(3:chunksize+2);
     
-    figure(1);    
+    figure(1);
     scatter(u1, u2, 'b.');
     
-    figure(2);    
+    figure(2);
     scatter3(u1, u2, u3, 'b.');
     
     last_x = x(chunksize+1);
 end
+
+figure(1);
+print('lcg_big_2d', '-depsc');
+figure(2);
+print('lcg_big_3d', '-depsc');
