7949a1c2aa8cdcaaf8d7b9adb92c51b6cab20784
Riccardo Zanol <ricc@zanol.eu>
2016-06-03 13:02:34 +0200

    Count the overflows

--- hw3/matlab/simulate_queue_2.m
+++ hw3/matlab/simulate_queue_2.m
@@ -1,7 +1,11 @@
-function [queue_size, delays] = simulate_queue_2(time_slots, b)
+function [queue_size, delays, overflows] = simulate_queue_2(time_slots, b, queue_lim)
+if nargin < 3
+    queue_lim = Inf;
+end
 queue_size = zeros(1, time_slots);
 delays = [];
 queue = [];
+overflows = [];
 for i=1:time_slots
     % Departure?
     if ~isempty(queue) && rand(1) <= b
@@ -13,7 +17,11 @@ for i=1:time_slots
     % Arrival?
     u = rand(1);
     if u <= 0.5
-        queue = [queue, i];
+        if length(queue) < queue_lim
+            queue = [queue, i];
+        else
+            overflows = [overflows, i];
+        end
     end
     
     queue_size(i) = length(queue);
