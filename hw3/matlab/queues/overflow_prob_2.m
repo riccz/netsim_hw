7ac45d3d0c09f114d1033e6a0b548ecf65b30051
Riccardo Zanol <ricc@zanol.eu>
2016-06-14 23:40:23 +0200

    Also compute the CI and check that there are enough overflows

--- hw3/matlab/queues/overflow_prob_2.m
+++ hw3/matlab/queues/overflow_prob_2.m
@@ -1,4 +1,18 @@
-function p = overflow_prob_2(queue_lim, b, slots)
+function [p, p_ci] = overflow_prob_2(queue_lim, b, slots, compute_ci)
 queue_sizes = simulate_queue_2(slots, b, queue_lim);
-p = sum(queue_sizes == queue_lim) / length(queue_sizes);
+
+z = sum(queue_sizes == queue_lim);
+n = length(queue_sizes);
+p = z/n;
+
+if ~exist('compute_ci', 'var') || ~compute_ci
+    p_ci = [];
+    return;
+end
+
+assert(z >= 6);
+assert(n-z >= 6);
+
+eta =  norminv((1+0.95)/2);
+p_ci = p + eta/n*sqrt(z*(1-p)) .* [-1, +1];
 end
