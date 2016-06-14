c7d4952fd5bfcde36fcaf9347e23ba010d64bf12
Riccardo Zanol <ricc@zanol.eu>
2016-06-14 23:41:38 +0200

    Update the queue size estimation

--- hw3/matlab/queue2size.m
+++ hw3/matlab/queue2size.m
@@ -6,15 +6,18 @@ stable_b = 2/3;
 p_over_target = 1e-5;
 slots = 30e5;
 queue_lim_inf = 0;
-queue_lim_sup = 100;
-check = @(queue_lim, b) sign(p_over_target - overflow_prob_2(queue_lim, b, slots));
+queue_lim_sup = 64;
 
-[qs, last_cmp] = binary_search(@(queue_lim) check(queue_lim, stable_b), queue_lim_inf, queue_lim_sup);
-if last_cmp < 0
+[qs, last_p] = binary_search(@(queue_lim) -overflow_prob_2(queue_lim, stable_b, slots), ...
+    -p_over_target, queue_lim_inf, queue_lim_sup);
+last_p = -last_p;
+if last_p < p_over_target
     qs = qs+1;
 end
 estimated_size = qs;
+[check_p, p_ci] = overflow_prob_2(estimated_size, stable_b, slots, true);
 
 fprintf('b = %f\n', stable_b);
 fprintf('  queue size = %d\n', estimated_size);
-fprintf('  prob overflow = %f\n', overflow_prob_2(estimated_size, stable_b, slots));
+fprintf('  prob overflow = %f\n', check_pi);
+fprintf('  CI = [%f, %f]\n', p_ci(:));
