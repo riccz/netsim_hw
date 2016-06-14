c522d3bdadc9bced32a27dd889cd063bafdc9033
Riccardo Zanol <ricc@zanol.eu>
2016-06-14 14:05:04 +0200

    Edit queue size

--- hw3/matlab/queue2size.m
+++ hw3/matlab/queue2size.m
@@ -2,25 +2,19 @@ close all; clear all; clc;
 addpath(genpath('./queues/'));
 addpath(genpath('./utils/'));
 
-bs = [1/2, 2/3];
+stable_b = 2/3;
 p_over_target = 1e-5;
 slots = 30e5;
-transient = 500;
 queue_lim_inf = 0;
-queue_lim_sup = 500;
-check = @(queue_lim, b) sign(p_over_target - overflow_prob_2(queue_lim, b, slots, transient));
+queue_lim_sup = 100;
+check = @(queue_lim, b) sign(p_over_target - overflow_prob_2(queue_lim, b, slots));
 
-estimated_sizes = zeros(1, length(bs));
-parfor i=1:length(bs)
-    [qs, last_cmp] = binary_search(@(queue_lim) check(queue_lim, bs(i)), queue_lim_inf, queue_lim_sup);
-    if last_cmp < 0
-        qs = qs+1;
-    end
-    estimated_sizes(i) = qs;
+[qs, last_cmp] = binary_search(@(queue_lim) check(queue_lim, stable_b), queue_lim_inf, queue_lim_sup);
+if last_cmp < 0
+    qs = qs+1;
 end
+estimated_size = qs;
 
-for i=1:length(bs)
-    fprintf('b = %f\n', bs(i));
-    fprintf('  queue size = %d\n', estimated_sizes(i));
-    fprintf('  prob overflow = %f\n', overflow_prob_2(estimated_sizes(i), bs(i), slots, transient));
-end
+fprintf('b = %f\n', stable_b);
+fprintf('  queue size = %d\n', estimated_size);
+fprintf('  prob overflow = %f\n', overflow_prob_2(estimated_size, stable_b, slots));
--- hw3/matlab/queues/overflow_prob_2.m
+++ hw3/matlab/queues/overflow_prob_2.m
@@ -1,4 +1,4 @@
-function p = overflow_prob_2(queue_lim, b, slots, transient)
-queue_sizes = simulate_queue_2(slots, b, queue_lim, transient);
+function p = overflow_prob_2(queue_lim, b, slots)
+queue_sizes = simulate_queue_2(slots, b, queue_lim);
 p = sum(queue_sizes == queue_lim) / length(queue_sizes);
 end
