fa9eab322263e9380e2e9105c5798fe7364b43d6
Riccardo Zanol <ricc@zanol.eu>
2016-06-23 18:17:49 +0200

    Handle the corner case r == D-1

--- hw3/matlab/geraf/inters_area.m
+++ hw3/matlab/geraf/inters_area.m
@@ -3,8 +3,17 @@ if length(D) ~= 1
     error('D can not be a vector');
 end
 
+zero_mask = (r == D-1);
+if any(zero_mask)
+    warning('found r == D-1');
+    nonzero_mask = not(zero_mask);
+    A = zeros(size(r, 1), size(r, 2));
+    A(nonzero_mask) = inters_area(r(nonzero_mask), D);
+    return;
+end
+
 assert(D >= 1);
-assert(all(and(r >= D-1, r <= D)));
+assert(all(and(r > D-1, r <= D)));
 
 w = (D^2 - r.^2 + 1) ./ (2*D);
 A = beta(w, 1) + beta(D-w, r);
