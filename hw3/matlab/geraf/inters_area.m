c3c26143ea41b722e62d063d8173b3bb4942b519
Riccardo Zanol <ricc@zanol.eu>
2016-06-23 17:14:50 +0200

    Check the input to the area calculation

--- hw3/matlab/geraf/inters_area.m
+++ hw3/matlab/geraf/inters_area.m
@@ -2,6 +2,10 @@ function A = inters_area(r, D)
 if length(D) ~= 1
     error('D can not be a vector');
 end
+
+assert(D >= 1);
+assert(all(and(r >= D-1, r <= D)));
+
 w = (D^2 - r.^2 + 1) ./ (2*D);
 A = beta(w, 1) + beta(D-w, r);
 end
