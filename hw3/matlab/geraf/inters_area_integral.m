15b08120800fe7f484b51e749d00742fa6687a8a
Riccardo Zanol <ricc@zanol.eu>
2016-06-24 14:29:39 +0200

    Compute the area with the integral

---
+++ hw3/matlab/geraf/inters_area_integral.m
@@ -0,0 +1,16 @@
+function A = inters_area_integral(r, D, samples)
+assert(length(r) == 1);
+assert(length(D) == 1);
+
+assert(D >= 1);
+assert(r >= D-1 && r <= D);
+
+u = rand(samples, 1);
+a = u .* (r-D+1) + D-1;
+
+assert(all(and(a >= D-1, a <= r)));
+
+w = (a.^2 + D^2 - 1) ./ (2 * a * D);
+f = 2 * a .* acos(w);
+A = (r-D+1) * mean(f);
+end
