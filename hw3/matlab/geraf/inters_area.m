25ee58e6bef0ce2924e0e1c5513d8167b75aeed0
Riccardo Zanol <ricc@zanol.eu>
2016-06-25 18:14:31 +0200

    Use the GQR to compute the intersection area

--- hw3/matlab/geraf/inters_area.m
+++ hw3/matlab/geraf/inters_area.m
@@ -1,28 +1,15 @@
 function A = inters_area(r, D)
-if length(D) ~= 1
-    error('D can not be a vector');
-end
-
-zero_mask = (r == D-1);
-if any(zero_mask)
-    warning('found r == D-1');
-    nonzero_mask = not(zero_mask);
-    A = zeros(size(r, 1), size(r, 2));
-    A(nonzero_mask) = inters_area(r(nonzero_mask), D);
-    return;
-end
+assert(length(r) == 1);
+assert(length(D) == 1);
 
 assert(D >= 1);
-assert(all(and(r > D-1, r <= D)));
+assert(r >= D-1 && r <= D);
 
-w = (D^2 - r.^2 + 1) ./ (2*D);
-A = beta(w, 1) + beta(D-w, r);
-
-assert(isreal(A));
-assert(all(A >= 0));
+f = @(a) integrand(a, D);
+A = lg_quad(f, D-1, r, 16);
 end
 
-function b = beta(x, y)
-x_y = x./y;
-b = 0.5 * (pi - 2*asin(x_y) - 2 * x_y .* sqrt(1 - x_y.^2));
+function y = integrand(a, D)
+w = (a.^2 + D^2 - 1) ./ (2 * a * D);
+y = 2 * a .* acos(w);
 end
