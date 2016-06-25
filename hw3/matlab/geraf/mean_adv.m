041ebc59ef0f241d099b0b5af8200b87b1aec53f
Riccardo Zanol <ricc@zanol.eu>
2016-06-25 18:31:46 +0200

    Revert "Fail if D<2"
    This reverts commit d435d41a86678a5b9e7990978ee606c8d3ba6b60.

--- hw3/matlab/geraf/mean_adv.m
+++ hw3/matlab/geraf/mean_adv.m
@@ -1,8 +1,4 @@
 function [E, varE] = mean_adv(D, M, samples)
-if D < 2
-    error('Before D=2 the results are wrong');
-end
-
 a = rand(samples, 1);
 A = inters_area(D-a, D);
 f = exp(-M .* A ./ pi);
