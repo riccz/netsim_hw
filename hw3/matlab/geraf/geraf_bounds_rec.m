9261023cdc5c6912e15b31aa345ff5923963e75d
Riccardo Zanol <ricc@zanol.eu>
2016-06-26 15:34:46 +0200

    Geraf recursive bounds are working

--- hw3/matlab/geraf/geraf_bounds_rec.m
+++ hw3/matlab/geraf/geraf_bounds_rec.m
@@ -3,33 +3,37 @@ lower = lower_bound(D, M, ni);
 upper = upper_bound(D, M, ni);
 end
 
-function [lower, hops] = lower_bound(D, M, ni)
+function lower = lower_bound(D, M, ni)
 n2 = zeros(D*ni, 1);
 n2(1:ni) = 1;
 for i=ni+1:D*ni
-    k = 1:ni;
-    w = adv_probability(i,k, ni, D, M);
-    w0 = exp(-M .* inters_area(i/ni, i/ni) ./ pi);
-    n2_rec = n2(i-ni+k-1);
+    w = zeros(1, ni);
+    for k = 1:ni;
+        w(k) = adv_probability(i, k, ni, D, M);
+    end
+    
+    n2_rec = n2(i - ni + (1:ni) -1);
     s = w * n2_rec;
+    w0 = zero_adv_probability(i, ni, D, M);
     n2(i) = (1 + s) / (1 - w0);
 end
 lower = n2(D*ni);
-hops = n2;
 end
 
-function [upper, hops] = upper_bound(D, M, ni)
+function upper = upper_bound(D, M, ni)
 n1 = zeros(D*ni, 1);
 n1(1:ni) = 1;
 for i=ni+1:D*ni
-    k = 1:ni-1;
-    w = adv_probability(i,k, ni, D, M);
-    w0 = exp(-M .* inters_area(i/ni, i/ni) ./ pi);
-    wini = adv_probability(i, ni, ni, D, M);
-    n1_rec = n1(i-ni+k);
+    w = zeros(1, ni-1);
+    for k = 1:ni-1;
+        w(k) = adv_probability(i, k, ni, D, M);
+    end
+    
+    n1_rec = n1(i - ni + (1:ni-1));
     s = w * n1_rec;
+    w0 = zero_adv_probability(i, ni, D, M);
+    wini = adv_probability(i, ni, ni, D, M);
     n1(i) = (1 + s) / (1 - w0 -wini);
 end
 upper = n1(D*ni);
-hops = n1;
 end
