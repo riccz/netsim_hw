9d760cd7309a29cc1be2809ada06447ca66d5f96
Riccardo Zanol <ricc@zanol.eu>
2016-06-28 16:24:39 +0200

    Add a script to plot the capture probabilities

---
+++ hw3/matlab/aloha.m
@@ -0,0 +1,25 @@
+close all; clear all; clc;
+addpath(genpath('./utils/'));
+addpath(genpath('./aloha/'));
+
+bs = [6, 10];
+ns = 1:30;
+nsim = 1e5;
+
+cn = zeros(length(ns), length(bs));
+cn_stddev = zeros(length(ns), length(bs));
+figure;
+hold on;
+leg = cell(length(bs), 1);
+for j=1:length(bs)
+    parfor i=1:length(ns)
+        [cn(i,j), cn_stddev(i,j)] = aloha_capture_sim(bs(j), ns(i), nsim);
+    end
+    errorbar_some(ns, cn(:,j), cn_stddev(:,j)/sqrt(nsim), 16, 1.96);
+    leg{j} = sprintf('b = %d dB', bs(j));
+end
+grid on;
+legend(leg{:})
+xlabel('n');
+ylabel('C_n');
+xlim([1, 30]);
