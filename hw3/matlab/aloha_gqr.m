dec6e074175c486b16942810e4f1040cb740eb2b
Riccardo Zanol <ricc@zanol.eu>
2016-06-29 09:54:26 +0200

    Separate the GQR computation from the plots

--- hw3/matlab/aloha_gqr.m
+++ hw3/matlab/aloha_gqr.m
@@ -5,27 +5,15 @@ addpath(genpath('./aloha/'));
 bs = [6, 10];
 ns = 1:30;
 
+% Capture probs.
 cn = zeros(length(ns), length(bs));
-figure(1);
-hold on;
-leg = cell(length(bs), 1);
 for j=1:length(bs)
     parfor i=1:length(ns)
         cn(i,j) = aloha_capture_gqr(bs(j), ns(i));
     end
-    plot(ns, cn(:,j));
-    leg{j} = sprintf('b = %d dB', bs(j));
 end
-grid on;
-legend(leg{:})
-xlabel('n');
-ylabel('C_n');
-xlim([1, 30]);
-print('aloha_capture_gqr', '-depsc');
 
-figure(2);
-hold on;
-leg = cell(length(bs), 1);
+% Throughput
 G = linspace(0, 30, 200);
 S = zeros(length(G), length(bs));
 for j=1:length(bs)
@@ -33,20 +21,36 @@ for j=1:length(bs)
         poiss = exp(-G(i)) * power(G(i), ns) ./ factorial(ns);
         S(i, j) = sum(transpose(poiss) .* cn(:,j));
     end
-    plot(G, S(:,j));
-    leg{j} = sprintf('b = %d dB', bs(j));
+end
+
+save('aloha_gqr_data.mat');
+
+% Plot the capture probs.
+figure;
+hold on;
+leg_cap = cell(length(bs), 1);
+for j=1:length(bs)
+    plot(ns, cn(:,j));
+    leg_cap{j} = sprintf('b = %d dB', bs(j));
 end
 grid on;
-legend(leg{:})
+legend(leg_cap{:})
+xlabel('n');
+ylabel('C_n');
+xlim([1, 30]);
+print('aloha_capture_gqr', '-depsc');
+
+% Plot the throughput
+figure;
+hold on;
+leg_thr = cell(length(bs), 1);
+for j=1:length(bs)
+    plot(G, S(:,j));
+    leg_thr{j} = sprintf('b = %d dB', bs(j));
+end
+grid on;
+legend(leg_thr{:})
 xlabel('G');
 ylabel('S');
 xlim([0, 30]);
 print('aloha_thr_gqr', '-depsc');
-
-save('aloha_gqr_workspace.mat');
-return
-load('aloha_sim_workspace.mat');
-figure(1);
-for j=1:length(bs)
-    
-end
