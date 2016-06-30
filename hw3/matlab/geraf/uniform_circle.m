acdea5c94f70d97bd1c9d0772eec689ba5e059d9
Riccardo Zanol <ricc@zanol.eu>
2016-06-30 13:11:15 +0200

    Generate points uniformly with specified R

--- hw3/matlab/geraf/uniform_circle.m
+++ hw3/matlab/geraf/uniform_circle.m
@@ -1,14 +1,17 @@
-function [x, y] = uniform_circle(nodes)
-x = rand(1, nodes) .* 2 -1;
-y = rand(1, nodes) .* 2 -1;
+function [x, y] = uniform_circle(nodes, R)
+if nargin < 2
+    R = 1;
+end
+x = rand(1, nodes) .* (2*R) -R;
+y = rand(1, nodes) .* (2*R) -R;
 while true
     d2 = x.^2 + y.^2;
-    del_i = find(d2 > 1);
+    del_i = find(d2 > R^2);
     if isempty(del_i)
         break;
     else
-        x(del_i) = rand(1, length(del_i));
-        y(del_i) = rand(1, length(del_i)) .* 2 -1 ;
+        x(del_i) = rand(1, length(del_i)) .* (2*R) -R;
+        y(del_i) = rand(1, length(del_i)) .* (2*R) -R;
     end
 end
 end
